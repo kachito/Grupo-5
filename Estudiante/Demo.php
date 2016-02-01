998a0b0c21902d0f745145db5d5043105c783586
fernandorizzo <fjavierchuqui@hotmail.com>
2016-01-31 22:18:15 -0500

    DemoCorregido
    Demo2

--- Estudiante/Demo.php
+++ Estudiante/Demo.php
@@ -23,16 +23,46 @@ class Demo
      }
     
      function setIdDemo($idDemo){
-       $this->idDemo = $idDemo;
+       $this->idEstudiante = $idEstudiante;
      } 
      function getIdDemo(){
-       return $this->idDemo;
+       return $this->idEstudiante;
      } 
      function setNombre($nombre){
        $this->nombre = $nombre;
      } 
      function getNombre(){
        return $this->nombre;
+     } 
+     function setApellido($apellido){
+       $this->apellido = $apellido;
+     } 
+     function getApellido(){
+       return $this->apellido;
+     } 
+     function setEdad($edad){
+       $this->edad = $edad;
+     } 
+     function getEdad(){
+       return $this->edad;
+     } 
+     function setCedula($cedula){
+       $this->cedula = $cedula;
+     } 
+     function getCedula(){
+       return $this->cedula;
+     } 
+     function setGenero($genero){
+       $this->genero = $genero;
+     } 
+     function getGenero(){
+       return $this->genero;
+     } 
+     function setEmail($email){
+       $this->email = $email;
+     } 
+     function getEmail(){
+       return $this->email;
      } 
 }
 ?>
