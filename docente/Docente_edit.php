e8487e3bf1af2ca5b472dcffb703d380a2fd9cec
kachito <lenin.yepez@gmail.com>
2016-02-15 14:21:03 -0500

    logo edit

--- docente/Docente_edit.php
+++ docente/Docente_edit.php
@@ -78,7 +78,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="index.php"><img src="images/logo2.png" alt="logo"></a>
+                    <a class="navbar-brand" href="index.php"><img src="../images/logo2.png" alt="logo"></a>
                 </div>
                 
                 <div class="collapse navbar-collapse navbar-right">
