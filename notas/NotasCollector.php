22f69d07b152d3deee992af126d48ade5bed0ae7
fernandorizzo <fjavierchuqui@hotmail.com>
2016-02-15 14:21:37 -0500

    actualizar

--- notas/NotasCollector.php
+++ notas/NotasCollector.php
@@ -29,20 +29,20 @@ class NotasCollector extends Collector
  
 
 
-  function updateDocente($id_nota,$anio,$nota1,$nota2,$nota3,$nota4,$promedio,$recuperacion) {
+  function updateNotas($id_nota,$anio,$nota1,$nota2,$nota3,$nota4,$promedio,$recuperacion) {
 	
 	//echo $cargo;
     $update = self::$db->getRow("Update nota set anio='$anio',nota1='$nota1',nota2='$nota2',nota3='$nota3',nota4='$nota4',promedio='$promedio',recuperacion='$recuperacion' where id_nota='$id_nota'");             
  return 1;  
   }
 
-  function deleteDocente($id) {
+  function deleteNotas($id) {
 
  $delete = self::$db->getRow("Delete from nota where id_profesor= '$id'");   
    return 1;          
   }
 
-  function insertDocente($anio,$nota1,$nota2,$nota3,$nota4,$promedio,$recuperacion) {
+  function insertNotas($anio,$nota1,$nota2,$nota3,$nota4,$promedio,$recuperacion) {
     $new_row = self::$db->getRow("Insert into nota (anio,nota1,nota2,nota3,nota4,promedio,recuperacion) values ('$anio','$nota1','$nota2','$nota3','$nota4','$promedio','$recuperacion'");
  return 1;               
   }
