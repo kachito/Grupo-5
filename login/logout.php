0636fa48312b3f7fdb0d3361674d556be8782560
kachito <lenin.yepez@gmail.com>
2016-02-14 19:34:43 -0500

    actualiza

---
+++ login/logout.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<title>Formulario</title>
+</head>
+
+<body>
+
+<?php
+
+	if (isset($_SESSION['usuario'])){
+ 	session_destroy();
+	echo "Ha cerrado sesion<br/>";
+	echo "<a href='../index.php'>Volver</a>";	
+	}
+	else
+ 	{ 
+	echo "Error<br/>";
+	echo "<a href='../index.php'>Volver</a>";	
+	}
+
+?>
+
+
+
+
+</body>
+</html>
