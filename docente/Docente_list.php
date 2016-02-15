0f917d9424e839418de7837d3a51f8e89e2e15aa
José Catagua <joesitop@gmail.com>
2016-02-15 13:48:36 -0500

    actualiza

--- docente/Docente_list.php
+++ docente/Docente_list.php
@@ -115,9 +115,9 @@ include_once("DocenteCollector.php");
 
                         <li><a href="../costruccion.html" class="dropdown-toggle" data-toggle="dropdown">Planificaciones <i class="fa fa-angle-down"></i></a>
                              <ul class="dropdown-menu">
-                                <li><a href="docente/form_docente.php">Docente</a></li>                                
+                                <li><a href="form_docente.php">Docente</a></li>                                
                                <!-- <li><a href="construccion.html">Alumnos</a></li> -->
-                               <li><a href="../form_Actividad.php">Actividades</a></li>
+                               <li><a href="../actividades/form_Actividad.php">Actividades</a></li>
                                <li><a href="../curso1/form_Curso.php">Cursos</a></li>
                                <li><a href="../notas/form_notas.php">Notas</a></li>
                             </ul>
